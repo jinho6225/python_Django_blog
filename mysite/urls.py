6ea37f0e4192e7ffcd44065f4cc27c5b5c00b865
jinho6225 <jinho6225@hotmail.com>
2020-05-31 17:57:39 -0700

    updated view login view logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
 
 from django.contrib.auth import views
+from django.conf.urls import url
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
